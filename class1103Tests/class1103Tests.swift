1a87ab9b8835046584a89e39b3a5ebbad4a159f8
Christinagithub <2605839246@qq.com>
2019-01-06 10:58:51 +0800

    Initial commit

---
+++ class1103Tests/class1103Tests.swift
@@ -0,0 +1,34 @@
+//
+//  class1103Tests.swift
+//  class1103Tests
+//
+//  Created by s20171105407 on 2018/11/3.
+//  Copyright © 2018 s20171105407. All rights reserved.
+//
+
+import XCTest
+@testable import class1103
+
+class class1103Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
